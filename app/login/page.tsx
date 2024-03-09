f67d56078d74301ffd8c07d54f3fea57ae697998
Benjamin Sengupta <benjaminsengupta@gmail.com>
2024-03-08 20:53:54 -0500

    Working login form UI

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,56 +1,100 @@
-import { SubmitButton } from "@/components/submit-button";
+"use client";
+
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useToast } from "@/components/ui/use-toast";
+
+const formSchema = z.object({
+  email: z.string().email("Please enter a valid email"),
+  password: z.string().min(1, "Please enter a password"),
+});
 
 export default function Login() {
-  async function login(formData: FormData) {
-    "use server";
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
+  const { toast } = useToast();
+
+  async function onSubmit(data: z.infer<typeof formSchema>) {
+    console.log(data);
+
+    // TODO: Call API
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    const data = Object.fromEntries(formData.entries());
-    console.log(data);
+    toast({
+      variant: "destructive",
+      title: "Not implemented!",
+    });
   }
 
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-      <form action={login}>
-        <div className="w-96 rounded-lg shadow-lg bg-white p-6 space-y-6 border border-gray-200 dark:border-gray-700">
-          <div className="space-y-2 text-center">
-            <h1 className="text-3xl font-bold">Login</h1>
-          </div>
-          <div className="space-y-2">
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                name="email"
-                placeholder="m@example.com"
-                required
-                type="email"
-              />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="w-96 rounded-lg shadow-lg bg-white p-6 space-y-6 border border-gray-200 dark:border-gray-700">
+            <div className="space-y-2 text-center">
+              <h1 className="text-3xl font-bold">Login</h1>
             </div>
             <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                name="password"
-                placeholder="Password"
-                required
-                type="password"
-              />
+              <div className="space-y-2">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="hi@example.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="space-y-2">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input type="password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
             </div>
-          </div>
 
-          <SubmitButton
-            className="w-full bg-black text-white"
-            variant="outline"
-            type="submit"
-          >
-            <div className="flex items-center justify-center">Login</div>
-          </SubmitButton>
-        </div>
-      </form>
+            <Button
+              className="w-full bg-black text-white"
+              variant="outline"
+              type="submit"
+              disabled={form.formState.isSubmitting}
+            >
+              <div className="flex items-center justify-center">Login</div>
+            </Button>
+          </div>
+        </form>
+      </Form>
     </div>
   );
 }
