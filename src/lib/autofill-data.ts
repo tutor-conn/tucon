deee36bd765944f4d1b601730ddd579cf5da7a97
TheAngryPopo <joelharder4@gmail.com>
2024-03-18 18:11:43 -0400

    forgot to add this file in prev commit :(

---
+++ src/lib/autofill-data.ts
@@ -0,0 +1,14 @@
+export const cities = [
+    {label: "Guelph", key: "guelph"},
+    {label: "Waterloo", key: "waterloo"},
+    {label: "Toronto", key: "toronto"},
+    {label: "lol", key: "lol"},
+    {label: "cringe", key: "cringe"},
+];
+
+
+export const countries = [
+    {label: "Canada", key: "canada"},
+    {label: "Belarus", key: "belarus"},
+    {label: "Hell", key: "hell"},
+]
