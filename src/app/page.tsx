9bb590250f31af8ae57965cb96ccb89362face8a
ericbuys <eric.buys02@gmail.com>
2024-03-24 12:21:49 -0400

    Merge conflicts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,8 +34,8 @@ function Home() {
     <div className="flex min-h-[100dvh] flex-col">
       <SiteHeader />
       <main className="flex-1">
-        <section className="w-full pt-12 md:pt-24 lg:pt-32">
-          <div className="container px-4 md:px-6">
+        <section className="w-full pt-12 md:pt-24 lg:pt-32 px-6 md:px-16 lg:px-24">
+          <div className="container px-0">
             <div className="grid gap-6 lg:grid-cols-[1fr_2fr]">
               <div className="flex flex-col justify-center space-y-2">
                 <div className="space-y-2">
@@ -43,8 +43,7 @@ function Home() {
                     Find the perfect tutor
                   </h1>
                   <p className="max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                    Personalized learning at your fingertips. Get matched with a
-                    tutor today.
+                    Personalized learning at your fingertips. Get matched with a tutor today.
                   </p>
                 </div>
                 <div className="min:[400px]:flex-row flex flex-col gap-2">
@@ -53,7 +52,7 @@ function Home() {
                   </Button>
                 </div>
               </div>
-              <div className="flex items-center justify-center">
+              <div className="flex items-center ml-auto">
                 <Image
                   alt="Hero"
                   className="aspect-video h-auto w-auto overflow-hidden rounded-xl object-cover object-center"
@@ -66,6 +65,25 @@ function Home() {
             </div>
           </div>
         </section>
+        {/* <section>
+          <div className="container">
+            <div className="flex flex-col justify-center">
+              <Image
+                alt="Tucon Logo"
+                src="/tucon-vertical.svg"
+                height={0}
+                width={0}
+                className="w-[30%] mx-auto"
+                priority
+              />
+              <h1 className="mx-auto font-bold text-4xl tracking-tighter text-secondary">Our Mission</h1>
+              <p className="w-[75%] mx-auto text-center text-xl">
+                Our goal is to provide a platform that seamlessly connects University of Guelph students with qualified tutors. 
+                We aim to simplify the tutor search process and provide a space where students can easily discover, chat, and engage with tutors.
+              </p>
+            </div>
+          </div>
+        </section> */}
       </main>
       <SiteFooter />
     </div>
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -20,7 +20,7 @@ export function SiteHeader() {
     : null;
 
   return (
-    <header className="flex h-14 items-center bg-background px-4 lg:px-6">
+    <header className="flex h-14 items-center bg-background justify-between pt-4 px-6 md:px-16 lg:px-24">
       <Link href={lastViewRoute ?? "/"}>
         <Image
           alt="Tucon Logo"
@@ -33,8 +33,6 @@ export function SiteHeader() {
       </Link>
       <span className="sr-only">Tucon</span>
       <nav className="flex flex-1 justify-between">
-        <div />
-
         <div className="flex gap-4 sm:gap-6">
           {isLoading && (
             <>
@@ -47,6 +45,26 @@ export function SiteHeader() {
 
           {data && data.userId !== null && <UserButtons userData={data} />}
         </div>
+      </nav>
+      <nav>
+        <ul className="flex ml-auto gap-2">
+          <li>
+            <Button asChild variant="ghost" size="sm">
+              <Link href="/about">About</Link>
+            </Button>
+          </li>
+          <li className="relative">
+            <span className="absolute inset-y-0 left-0 w-0.5 bg-gray-300" aria-hidden="true"></span>
+            <Button className="ml-2" asChild variant="ghost" size="sm">
+              <Link href="/sign-up">Sign up</Link>
+            </Button>
+          </li>
+          <li>
+            <Button asChild variant="ghost" size="sm">
+              <Link href="/login">Log in</Link>
+            </Button>
+          </li>
+        </ul>
       </nav>
     </header>
   );
